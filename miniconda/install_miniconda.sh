350a0c1f03dafb88338e7fee757be04416e161d5
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-30 13:15:16 -0400

    miniconda/install_miniconda.sh: Install dependencies of conda's Qt4.

--- miniconda/install_miniconda.sh
+++ miniconda/install_miniconda.sh
@@ -6,6 +6,9 @@ apt-get install -y wget
 # Install bzip2 and tar. Needed for decompressing packages.
 apt-get install -y bzip2 tar
 
+# Install dependencies of conda's Qt4.
+apt-get install -y libsm6 libXrender1
+
 # Download and configure conda.
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p /opt/conda
